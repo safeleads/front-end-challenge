8c6ea053f7f639386fe073d066dab1cbc5c45183
Iago <yagomello16@gmail.com>
2020-04-20 22:52:04 -0300

    add OneChoiseButton

---
+++ src/components/common/OneChoiseButton.vue
@@ -0,0 +1,54 @@
+<template>
+  <b-form-group class="one-choise-button" :label="label" label-cols-sm="5">
+    <b-form-radio-group :id="name" :name="name" buttons>
+      <b-form-radio
+        v-for="(opt, i) in options"
+        :key="`__one-choise-button__${i}__`"
+        :value="opt.value"
+        button-variant="blank"
+      >
+        <b-img :src="opt.img" :alt="opt.alt" />
+        {{ opt.text }}
+      </b-form-radio>
+    </b-form-radio-group>
+  </b-form-group>
+</template>
+
+<script>
+export default {
+  name: 'OneChoiseButton',
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    label: {
+      type: String,
+      default: 'Label'
+    },
+    options: {
+      type: Array,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.one-choise-button {
+  label.btn {
+    background-color: #dde1e6;
+    color: #717277;
+
+    img {
+      width: 13px;
+      height: 15px;
+    }
+
+    @include hover-focus-active {
+      background-color: $primary;
+      color: white;
+    }
+  }
+}
+</style>
--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,4 +1,7 @@
 import TheSidebar from './TheSidebar.vue';
 import TheHeader from './TheHeader.vue';
 
-export { TheSidebar, TheHeader };
+import BodyTabs from './BodyTabs.vue';
+import OneChoiseButton from './OneChoiseButton.vue';
+
+export { TheSidebar, TheHeader, BodyTabs, OneChoiseButton };
