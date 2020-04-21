f87f89b03f89b772284eaaffc6b6c7fe93e39e62
Iago <yagomello16@gmail.com>
2020-04-20 22:53:32 -0300

    adding kanban partials

---
+++ src/views/home/tabs/kanban/Kanban.vue
@@ -0,0 +1,53 @@
+<template>
+  <b-container class="kanban" tag="section">
+    <b-row class="kanban__header" tag="header" align-v="center">
+      <b-col md="6">
+        <OneChoiseButton
+          class="mb-0"
+          name="kanban-type"
+          label="Mudar para kanban de:"
+          :options="kanbanTypeOptions"
+        />
+      </b-col>
+      <b-col md="6">
+        <b-btn class="btn-filter" variant="dark">
+          <img
+            :src="require('~/assets/icons/filtrar.svg')"
+            alt="Icone de filtragem."
+          />
+          Filtrar por
+        </b-btn>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+import { OneChoiseButton } from '~/components/common';
+export default {
+  name: 'Kanban',
+  components: {
+    OneChoiseButton
+  },
+  data() {
+    return {
+      kanbanTypeOptions: [
+        {
+          text: 'Leads',
+          value: 'leads',
+          img: require('~/assets/icons/person.svg'),
+          alt: 'Icone indicando Kanban Leads'
+        },
+        {
+          text: 'Projeto',
+          value: 'projeto',
+          img: require('~/assets/icons/projeto.svg'),
+          alt: 'Icone indicando Kanban Projeto'
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
