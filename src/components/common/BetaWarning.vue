3250d6d96d7db03c3a1e177b57539ed090271b11
Iago <yagomello16@gmail.com>
2020-04-21 10:46:08 -0300

    add BetaWarning component

---
+++ src/components/common/BetaWarning.vue
@@ -0,0 +1,37 @@
+<template>
+  <b-row class="beta-warning">
+    <b-col class="beta-warning__version text-right" md="6" lg="4">
+      Versão Beta
+    </b-col>
+    <b-col class="beta-warning__development text-right" md="6" lg="8">
+      Tela em desenvolvimento
+      <img
+        style="margin-left: 12px;"
+        :src="require('~/assets/icons/desenvolvimento.svg')"
+        alt="Icone de tela em versão beta."
+      />
+    </b-col>
+  </b-row>
+</template>
+
+<script>
+export default {
+  name: 'BetaWarning'
+};
+</script>
+
+<style lang="scss" scoped>
+.beta-warning {
+  max-width: 68%;
+  margin: 0 0 0 -5px;
+  background-color: #e7ebf1;
+
+  > [class*='col-'] {
+    padding: 11px 14px;
+  }
+
+  &__version {
+    background-color: $primary;
+  }
+}
+</style>
--- src/components/common/index.js
+++ src/components/common/index.js
@@ -3,5 +3,6 @@ import TheHeader from './TheHeader.vue';
 
 import BodyTabs from './BodyTabs.vue';
 import OneChoiseButton from './OneChoiseButton.vue';
+import BetaWarning from './BetaWarning.vue';
 
-export { TheSidebar, TheHeader, BodyTabs, OneChoiseButton };
+export { TheSidebar, TheHeader, BodyTabs, OneChoiseButton, BetaWarning };
