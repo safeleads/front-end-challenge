5240c4e0974e114e3253088b8d3c1316c29ed7b8
Iago <yagomello16@gmail.com>
2020-04-21 21:00:39 -0300

    add LeadasCardProjectAmount partial

---
+++ src/views/home/tabs/kanban/partials/kanban-leads/LeadsCardProjectAmount.vue
@@ -0,0 +1,43 @@
+<template>
+  <div
+    class="leads-card-project-amount align-center justify-content-center justify-content-sm-start"
+  >
+    <b-img
+      class="leads-card-project-amount--icon"
+      :src="require('~/assets/icons/projeto.svg')"
+      alt="icone de quantidades de projetos."
+    />
+    <span
+      class="leads-card-project-amount--label text-10 line-height-14 text-primary-alt"
+    >
+      QTD. Projeto:
+    </span>
+    <span
+      class="leads-card-project-amount--quantity text-12 line-height-17 font-weight-bold"
+      v-text="projects"
+    ></span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LeadsCardProjectAmount',
+  props: {
+    /**
+     * quantidade de projetos
+     */
+    projects: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.leads-card-project-amount {
+  > *:not(:last-child) {
+    margin-right: 5px;
+  }
+}
+</style>
