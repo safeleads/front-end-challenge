244010589d8d9ded854c427e0c131c8c20b7f6b3
Iago <yagomello16@gmail.com>
2020-04-21 21:00:27 -0300

    add LeadasCardSimulation partial

---
+++ src/views/home/tabs/kanban/partials/kanban-leads/LeadsCardSimulation.vue
@@ -0,0 +1,56 @@
+<template>
+  <b-row
+    class="leads-card-simulation text-12 line-height-17 mx-0"
+    align-h="start"
+    no-gutters
+  >
+    <b-col class="flex-center" sm="auto">
+      <b-img
+        class="leads-card-simulation--icon"
+        :src="require('~/assets/icons/unidade_consumidora.svg')"
+        alt="Imagem da simulação do valor de uma tarefa."
+      />
+      <span class="leads-card-simulation--label">Valor da simulação:</span>
+    </b-col>
+
+    <b-col
+      class="leads-card-simulation--money text-center font-weight-bold"
+      sm="auto"
+    >
+      {{ simulation | currency }}
+    </b-col>
+  </b-row>
+</template>
+
+<script>
+export default {
+  name: 'LeadsCardSimulation',
+  props: {
+    simulation: {
+      type: Number,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.leads-card-simulation {
+  padding: 12px 17px;
+  background-color: #e7ebf1;
+
+  &--icon {
+    margin-right: 5px;
+  }
+
+  [class*='col-']:first-child {
+    @include media-breakpoint-up(sm) {
+      margin-right: 5px;
+    }
+  }
+
+  &--label {
+    color: #6a1476;
+  }
+}
+</style>
